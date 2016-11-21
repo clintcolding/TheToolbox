4696d2b0728e01ba8bc08c3bfbb7faa29c1fb14c
Clint Colding <clintcolding@gmail.com>
2016-11-21 15:39:55 -0500

    Added try/catch.

--- Functions/Get-SysInfo.ps1
+++ Functions/Get-SysInfo.ps1
@@ -1,39 +1,70 @@
-﻿function Get-Sysinfo{
+﻿<#
+.Synopsis
+   Gathers local or remote system information.
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   PS C:\Windows\system32> Get-ADComputer -Filter 'OperatingSystem -like "*server*"' | get-sysinfo | ft
+
+   ComputerName Description     OS                                         Asset Tag CPU Memory Manufacturer Model 
+   ------------ -----------     --                                         --------- --- ------ ------------ ----- 
+   DC01         Primary DC      Microsoft Windows Server 2008 R2 Standard  VMware      1      4 VMware, Inc. VMw...
+   TS01         Terminal Server Microsoft Windows Server 2008 R2 Standard  VMware      2     12 VMware, Inc. VMw...
+
+   Builds server list from AD and then gathers system info.
+.EXAMPLE
+   
+#>
+function Get-Sysinfo{
 
     [CmdletBinding()]
 
     Param(
         
 
-        [Alias("Name")]
-        [string[]]$computername
+        [Parameter(ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string[]]$Name='localhost'
 )
 
     Process{
+        
+        try{
+            foreach($Computer in $Name){
 
-        foreach($computer in $computername){
+                $Asset = Get-WmiObject Win32_BIOS -ComputerName $Computer -ErrorAction Stop -ErrorVariable AssetError
+                $CPU = Get-WmiObject Win32_Processor -ComputerName $Computer -ErrorAction Stop -ErrorVariable CPUError
+                $OS = Get-WmiObject Win32_OperatingSystem -ComputerName $Computer -ErrorAction Stop -ErrorVariable OSError
+                $System = Get-WmiObject Win32_ComputerSystem -ComputerName $Computer -ErrorAction Stop -ErrorVariable SysError
 
-            $Asset = Get-WmiObject Win32_BIOS -ComputerName $computer
-            $CPU = Get-WmiObject Win32_Processor -ComputerName $computer
-            $OS = Get-WmiObject Win32_OperatingSystem -ComputerName $computer
-            $System = Get-WmiObject Win32_ComputerSystem -ComputerName $computer
+                $Prop = [ordered]@{
 
-            $x = [ordered]@{
-
-                  'ComputerName' = $OS.PSComputerName
-                  'Description'  = $OS.Description
-                  'OS'           = $OS.Caption
-                  'Asset Tag'    = if($Asset.SerialNumber -like "*VMware*"){'VMware'}
-                                   else{$Asset.SerialNumber}
-                  'CPU'          = ($CPU.NumberOfCores | Measure-Object -Sum).Sum
-                  'Memory'       = [math]::Round($System.TotalPhysicalMemory / 1GB)
-                  'Manufacturer' = $System.Manufacturer
-                  'Model'        = $System.Model      
+                      'ComputerName' = $OS.PSComputerName
+                      'Description'  = $OS.Description
+                      'OS'           = $OS.Caption
+                      'Asset Tag'    = if($Asset.SerialNumber -like "*VMware*"){'VMware'}
+                                       else{$Asset.SerialNumber}
+                      'CPU'          = ($CPU.NumberOfCores | Measure-Object -Sum).Sum
+                      'Memory'       = [math]::Round($System.TotalPhysicalMemory / 1GB)
+                      'Manufacturer' = $System.Manufacturer
+                      'Model'        = $System.Model      
       
-                  }
+                      }
 
-            $Obj=New-Object -TypeName PSObject -Property $x
-            Write-Output $Obj
+                $Obj=New-Object -TypeName PSObject -Property $Prop
+                Write-Output $Obj
+            }
+        }
+
+        catch [System.Runtime.InteropServices.COMException]{
+            Write-Warning "Could not connect to $Computer."    
+        }
+        
+        catch{
+            Write-Error $AssetError
+            Write-Error $CPUError
+            Write-Error $OSError
+            Write-Error $SysError
         }
     }
 }
