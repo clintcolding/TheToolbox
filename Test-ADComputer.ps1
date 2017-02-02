056897aebcf683b591bee701d719785c8a8cf4b9
Clint Colding <clintcolding@gmail.com>
2017-02-02 17:20:11 -0500

    Initial commit

---
+++ Test-ADComputer.ps1
@@ -0,0 +1,57 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Test-ADComputer
+{
+    [CmdletBinding()]
+
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Filter,
+
+        $ComputerName
+    )
+
+    Begin
+    {
+        $Computers = Get-ADComputer -Filter *
+    }
+    Process
+    {
+        foreach ($Computer in $Computers) {
+            $Test = Test-Connection -ComputerName $Computer.Name -Count 2 -Quiet
+    
+            if ($Test -eq $false){
+                $Prop = [ordered] @{
+                    'ComputerName' = $Computer.Name
+                    'Status'       = "Offline"
+                }
+            }
+
+            if ($Test -eq $true){
+                $Prop = [ordered] @{
+                    'ComputerName' = $Computer.Name
+                    'Status'       = "Online"
+                }
+            }
+
+        $Obj = New-Object -TypeName PSObject -Property $Prop
+
+            Write-Output $Obj
+        }
+    }
+    End
+    {
+    }
+}
