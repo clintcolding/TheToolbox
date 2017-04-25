e7ba46093d8ed5bb730b4d3e58963777e9ee8dd9
Clint Colding <clintcolding@gmail.com>
2017-04-25 16:15:33 -0400

    Added script

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -1,5 +1,22 @@
-﻿# Exchange Mailbox Forward #
+﻿### Exchange Mailbox Forward ###
 
 Set-Mailbox -Identity user -DeliverToMailboxAndForward $true -ForwardingSMTPAddress "user@company.com"
     
 Set-Mailbox -Identity user -DeliverToMailboxAndForward $false -ForwardingSmtpAddress $null
+
+### Get Hostname from IP ###
+
+$path = Import-Csv c:\bin\iplist.txt
+
+foreach ($entry in $path) {
+    $Hostname = Resolve-DnsName $entry.ip
+
+    $Prop = [ordered] @{
+        'Hostname' = $Hostname.NameHost
+        'IP'       = $entry.ip
+    }
+    
+    $Obj = New-Object -TypeName PSObject -Property $Prop
+
+    Write-Output $Obj
+}
