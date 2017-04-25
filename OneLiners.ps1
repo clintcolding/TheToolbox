b000bbdcc4276948364d2e8e1f96b95e7053f8e9
Clint Colding <clintcolding@gmail.com>
2017-04-25 16:08:17 -0400

    Initial commit

---
+++ OneLiners.ps1
@@ -0,0 +1,5 @@
+﻿# Exchange Mailbox Forward #
+
+Set-Mailbox -Identity user -DeliverToMailboxAndForward $true -ForwardingSMTPAddress "user@company.com"
+    
+Set-Mailbox -Identity user -DeliverToMailboxAndForward $false -ForwardingSmtpAddress $null
