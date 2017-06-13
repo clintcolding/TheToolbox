d3edf9deca90140188fe34bd00ccdfc40f8f0d32
Clint Colding <clintcolding@gmail.com>
2017-06-13 09:06:17 -0400

    Added help

--- Test-Service.ps1
+++ Test-Service.ps1
@@ -1,24 +1,15 @@
 ﻿<#
 .Synopsis
-   Short description
+   Tests and corrects the status of a service.
 .DESCRIPTION
-   Long description
+   Determines if a service is running or stopped. If stopped, Test-Service will attempt to start.
 .EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
-.INPUTS
-   Inputs to this cmdlet (if any)
-.OUTPUTS
-   Output from this cmdlet (if any)
-.NOTES
-   General notes
-.COMPONENT
-   The component this cmdlet belongs to
-.ROLE
-   The role this cmdlet belongs to
-.FUNCTIONALITY
-   The functionality that best describes this cmdlet
+   C:\> Test-Service -ComputerName DC01,DC02,DC03 -Service DNS -Verbose
+   VERBOSE: Testing DNS service status.
+   VERBOSE: DNS service is running on DC01
+   VERBOSE: DNS service is running on DC02
+   WARNING: DNS service is stopped on DC03. Starting service...
+   VERBOSE: DNS service is running on DC03
 #>
 function Test-Service
 {
