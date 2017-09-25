7a273eeb3f959d41e579d5fc5669295465c396f9
Clint Colding <ccolding@a1hr.com>
2017-09-25 10:49:09 -0400

    Added Round method from Math class

--- Get-FreeSpace.ps1
+++ Get-FreeSpace.ps1
@@ -4,11 +4,11 @@
 .DESCRIPTION
    Displays available free space on local drives. System Reserved and Recovery drives are excluded.
 .EXAMPLE
-   Get-FreeSpace | Where-Object {[int]$_."Free(GB)" -lt "10"}
+   Get-FreeSpace | Where-Object {$_."Free(GB)" -lt "10"}
 
    Returns computers with less than 10 GB free space.
 .EXAMPLE
-   Get-FreeSpace | Where-Object {[int]$_."Free(%)" -lt "20"}
+   Get-FreeSpace | Where-Object {$_."Free(%)" -lt "20"}
 
    Returns computers with less than 20% free space.
 .EXAMPLE
@@ -24,7 +24,7 @@ function Get-FreeSpace
         [Alias('ComputerName')]
         [Parameter(ValueFromPipelineByPropertyName=$true,
                    Position=0)]
-        [String[]]$Name='localhost'
+        [String[]]$Name = 'localhost'
     )
     Begin
     {
@@ -35,21 +35,21 @@ function Get-FreeSpace
         Foreach($Computer in $Name)
         {
             Try{
-                $volume=Get-WmiObject -ComputerName $Computer Win32_Volume -ErrorAction Stop -ErrorVariable GFSError
+                $volume = Get-WmiObject -ComputerName $Computer Win32_Volume -ErrorAction Stop -ErrorVariable GFSError
         
                 Foreach($drive in $volume){
                     If ($drive.drivetype -eq 3 -and $drive.label -ne "System Reserved" -and $drive.label -ne "Recovery"){
                     
-                        $Prop=[ordered]@{
+                        $Prop = [ordered]@{
                             'ComputerName' = $Computer
                             'Drive'        = $drive.name
                             'Description'  = $drive.label
-                            'Total(GB)'    = ($drive.capacity / 1GB).ToString("F0")
-                            'Free(GB)'     = ($drive.freespace / 1GB).ToString("F1")
-                            'Free(%)'      = (($drive.freespace / $drive.capacity)*100).ToString("F1")
+                            'Total(GB)'    = [math]::round($drive.capacity / 1GB)
+                            'Free(GB)'     = [math]::round($drive.freespace / 1GB)
+                            'Free(%)'      = [math]::round(($drive.freespace / $drive.capacity)*100)
                         }
       
-                     $Obj=New-Object -TypeName PSObject -Property $Prop
+                     $Obj = New-Object -TypeName PSObject -Property $Prop
                      Write-Output $Obj
 
                     }
