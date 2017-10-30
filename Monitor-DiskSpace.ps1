f85caac734c25033ec0a4baaf1075243d3731862
Clint Colding <ccolding@a1hr.com>
2017-10-30 15:26:46 -0400

    Deleted

--- Monitor-DiskSpace.ps1
+++
@@ -1,8 +0,0 @@
-$volume = Get-WmiObject -ComputerName 100MAIL01 Win32_Volume
-
-Foreach($drive in $volume){
-    If ($drive.drivetype -eq 3 -and $drive.label -ne "System Reserved" -and $drive.label -ne "Recovery"){
-        [int]$freepct = ($drive.freespace / $drive.capacity)*100
-        write-output $drive.name $freepct
-    }
-}
--- console.ps1
+++
@@ -1,9 +0,0 @@
-while ($true) {
-    $CPU = (Get-WmiObject Win32_Processor).LoadPercentage
-    $os = Get-Ciminstance Win32_OperatingSystem
-    $pctFree = [math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize)*100)
-    $RAM = 100 - $pctFree
-    
-    Write-Host -NoNewLine "`rCPU Usage: $CPU%   RAM Usage: $RAM%"
-    Start-Sleep -Milliseconds 3000
-}
