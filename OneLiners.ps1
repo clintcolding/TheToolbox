fa60b7ead5ccc9589b0e96bd5fb37010e6a773ce
Clint Colding <ccolding@a1hr.com>
2017-10-30 15:32:08 -0400

    Added VMware script

--- OneLiners.ps1
+++ OneLiners.ps1
@@ -30,3 +30,13 @@ Get-HardDisk -VM 100WSUS01 | Set-HardDisk -CapacityGB 400 -ResizeGuestPartition
 $user = 'ccolding'
 
 Get-ADPrincipalGroupMembership $user | Select-Object -Property Name, GroupScope, GroupCategory | Sort-Object -Property Name | Format-Table -A
+
+### Find VMs that have outdated VM Tools ###
+
+foreach ($vm in Get-VM) {
+    if ($vm.PowerState -eq 'PoweredOn'){
+        if ($vm.ExtensionData.Guest.ToolsStatus -eq 'toolsOld') {
+            $vm.name
+        }
+    }
+}
