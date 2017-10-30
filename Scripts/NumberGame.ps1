96400b569c5966fb6a37619510791d86766d8fef
Clint Colding <ccolding@a1hr.com>
2017-10-30 15:41:37 -0400

    Moved and renamed

---
+++ Scripts/NumberGame.ps1
@@ -0,0 +1,29 @@
+function Start-NumberGame {
+    $number = Get-Random -Minimum 1 -Maximum 100
+    $guessattempts = 1
+
+    Write-Output "I've chosen a random number from 1 to 100, can you guess it?"
+
+    while ($true) {
+        $guess = Read-Host -Prompt "What's your guess?"
+
+        if ($guess -eq $number) {
+            if ($guessattempts -ne 1) {
+                Write-Output "After $guessattempts guesses, you win!"
+                break
+            }
+            else {
+                Write-Output "After $guessattempts guess, you win!"
+                break
+            }
+        }
+        if ($guess -lt $number) {
+            Write-Output "Higher!"
+            $guessattempts += 1
+        }
+        else {
+            Write-Output "Lower!"
+            $guessattempts += 1
+        }
+    }
+}
